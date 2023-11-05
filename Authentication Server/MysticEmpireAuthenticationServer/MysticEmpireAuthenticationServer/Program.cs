f7a68ee1085e392799ebc6b9f0c28be82974d498
A Col <aaron.coltof@gmail.com>
2023-11-05 17:35:00 +0100

    [add] authserver server side done

--- Authentication Server/MysticEmpireAuthenticationServer/MysticEmpireAuthenticationServer/Program.cs
+++ Authentication Server/MysticEmpireAuthenticationServer/MysticEmpireAuthenticationServer/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Threading;
 
 namespace MysticEmpireAuthenticationServer
 {
-    internal class Program
+    class Program
     {
+        private static bool isRunning = false;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Mystic Empire Authentication Server, all right reserved 2023©. ");
+            Console.WriteLine("Mystic Empire Authentication Server");
+            Console.Title = "MysticEmpireAuthenticationServer";
+            isRunning = true;
+
+            Thread mainThread = new Thread(new ThreadStart(MainThread));
+            mainThread.Start();
+
+            Server.Start(1000, 26950);
+        }
+
+        private static void MainThread()
+        {
+            Console.WriteLine($"Main thread started. Running at {Constants.TICKS_PER_SEC} ticks per second.");
+            DateTime _nextLoop = DateTime.Now;
+
+            while (isRunning)
+            {
+                while (_nextLoop < DateTime.Now)
+                {
+                    GameLogic.Update();
+
+                    _nextLoop = _nextLoop.AddMilliseconds(Constants.MS_PER_TICK);
+
+                    if (_nextLoop > DateTime.Now)
+                    {
+                        Thread.Sleep(_nextLoop - DateTime.Now);
+                    }
+                }
+            }
         }
     }
 }
